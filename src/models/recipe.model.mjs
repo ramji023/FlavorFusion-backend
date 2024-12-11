d35d848df47e3e22004eba3f8cf979765b9b82e3
Ram ji <mishraramji039@gmail.com>
2024-12-11 15:25:49 +0530

    design recipe model

--- src/models/recipe.model.mjs
+++ src/models/recipe.model.mjs
@@ -2,7 +2,47 @@ import mongoose, { Schema } from "mongoose";
 
 
 const recipeSchema = new Schema({
-
-});
+    recipeTitle: {
+        type: String,
+        required: [true, "title is required."],
+    },
+    recipeVideo: {
+        type: String,
+    },
+    description: {
+        type: String,
+        default: "No description provided"
+    },
+    ingredients: [
+        {
+            type: String,
+            required: true,
+        }
+    ],
+    instructions: [
+        {
+            stepNumber: { type: Number, required: true },
+            text: { type: String, required: true },
+        }
+    ],
+    prepTime: {
+        type: String,
+        required: true,
+    },
+    cookTime: {
+        type: String,
+        required: true,
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    images: [
+        {
+            type: String,
+        }
+    ]
+}, { timestamps: true });
 
 export const Recipe = mongoose.model("Recipe", recipeSchema);
